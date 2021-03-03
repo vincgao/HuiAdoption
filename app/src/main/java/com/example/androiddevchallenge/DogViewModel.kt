b40e385128b5a25a2c3e9105c50eb65b56867742
Vincent <gaobing1230@gmail.com>
2021-03-03 08:05:43 +0800

    add dogs data

---
+++ app/src/main/java/com/example/androiddevchallenge/DogViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.androiddevchallenge
+
+import androidx.lifecycle.ViewModel
+
+class DogViewModel : ViewModel() {
+
+
+
+
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/data/DogsData.kt
@@ -0,0 +1,25 @@
+package com.example.androiddevchallenge.data
+
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.model.Breed
+import com.example.androiddevchallenge.model.Dog
+import com.example.androiddevchallenge.model.Gender
+
+val dogs = listOf(
+    Dog(id = 1,
+        name = "sijia",
+        gender = Gender.GIRL,
+        photoRes = R.drawable.sijia,
+        age = null,
+        breed = Breed.Labrador,
+        background = "rescued in the dog meat car",
+    ),
+    Dog(id = 2,
+        name = "siji",
+        gender = Gender.GIRL,
+        photoRes = R.drawable.siji,
+        age = null,
+        breed = Breed.Labrador,
+        background = "rescued from the dog dealer",
+    )
+)
