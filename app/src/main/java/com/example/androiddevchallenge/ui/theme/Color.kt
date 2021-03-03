14cc4e2f717245ee802d2e71d9d7a270cb47eb0e
Vincent <gaobing1230@gmail.com>
2021-03-03 08:19:54 +0800

    update theme

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -17,7 +17,7 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val red200 = Color(0xFFFF867F)
+val red500 = Color(0xFFFF5252)
+val red700 = Color(0xFFC50E29)
+val pink200 = Color(0xFFFFC1E3)
